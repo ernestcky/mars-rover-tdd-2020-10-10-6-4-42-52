5cb20729f0a2e43aa41b4b951e7a1a60c3906971
Ernest Cheung <ernest.cheung@oocl.com>
2020-11-26 17:58:02 +0800

    [Rover][Ernest] Test: should return y-1 when controlRover given [0,0,S] and M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void should_return_y_plus_1_when_executeCommands_given_0_0_N_and_M() {
+    void should_return_y_plus_1_when_controlRover_given_0_0_N_and_M() {
         // given
         Rover rover = new Rover(0, 0, "N");
 
@@ -18,4 +18,19 @@ class DemoTest {
         assertEquals(1, rover.getLocationY());
         assertEquals("N", rover.getDirection());
     }
+    
+    @Test
+    public void should__return_y_minus_1_when_controlRover_given_0_0_S_and_M() {
+        // given
+        Rover rover = new Rover(0, 0, "S");
+
+        // when execute
+        rover.controlRover("M");
+
+        // then
+        assertEquals(0, rover.getLocationX());
+        assertEquals(-1, rover.getLocationY());
+        assertEquals("S", rover.getDirection());
+    }
+    
 }
