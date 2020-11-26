aafc7779a3091dfff9186363e1ec46b346997508
Ernest Cheung <ernest.cheung@oocl.com>
2020-11-26 17:51:28 +0800

    [Rover][Ernest] Test: should return y+1 when controlRover given [0,0,N] and M

---
+++ src/main/java/com/afs/tdd/Rover.java
@@ -0,0 +1,32 @@
+package com.afs.tdd;
+
+import java.util.Arrays;
+
+public class Rover {
+    private Integer locationX;
+    private Integer locationY;
+    private String direction;
+
+    public Rover(Integer locationX, Integer locationY, String direction) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
+
+    public Integer getLocationX() {
+        return locationX;
+    }
+
+    public Integer getLocationY() {
+        return locationY;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void controlRover(String instruction) {
+
+    }
+
+}
