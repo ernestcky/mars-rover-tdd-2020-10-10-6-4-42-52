b6b5ffc2a262ff3006fddf81c1822fc6ed38edc3
Ernest Cheung <ernest.cheung@oocl.com>
2020-11-26 18:05:31 +0800

    [Rover][Ernest] Test: should return x+1 when controlRover given [0,0,E] and M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -20,7 +20,7 @@ class DemoTest {
     }
     
     @Test
-    public void should__return_y_minus_1_when_controlRover_given_0_0_S_and_M() {
+    public void should_return_y_minus_1_when_controlRover_given_0_0_S_and_M() {
         // given
         Rover rover = new Rover(0, 0, "S");
 
@@ -32,5 +32,20 @@ class DemoTest {
         assertEquals(-1, rover.getLocationY());
         assertEquals("S", rover.getDirection());
     }
+
+    @Test
+    public void should_return_x_plus_1_when_controlRover_given_0_0_E_and_M() {
+        // given
+        Rover rover = new Rover(0, 0, "E");
+
+        // when execute
+        rover.controlRover("M");
+
+        // then
+        assertEquals(1, rover.getLocationX());
+        assertEquals(0, rover.getLocationY());
+        assertEquals("E", rover.getDirection());
+    }
+
     
 }
